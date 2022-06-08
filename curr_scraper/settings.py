c32d9713d65c46abb96c61e31b3b037e92955b5d
Fazzer6132 <50322696+Fazzer6132@users.noreply.github.com>
2022-06-09 00:18:45 +0300

    Update settings.py

--- curr_scraper/settings.py
+++ curr_scraper/settings.py
@@ -69,8 +69,8 @@ WSGI_APPLICATION = 'curr_scraper.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'DATABASE_URL': os.environ.get('DATABASE_URL'),
     }
 }
 
