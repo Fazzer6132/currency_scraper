ef89a4670e391d6150cb3f54acf4989ef3502904
Fazzer6132 <50322696+Fazzer6132@users.noreply.github.com>
2022-06-24 20:01:35 +0300

    Update models.py

--- curr_scraper/apps/api_caller/models.py
+++ curr_scraper/apps/api_caller/models.py
@@ -2,19 +2,25 @@ from django.db import models
 
 
 class Currency(models.Model):
-    code = models.TextField()
-    description = models.TextField()
+    """A model describing type of currency."""
+    code = models.CharField()
+    description = models.CharField()
 
     class Meta:
         app_label = "api_caller"
+        verbose_name = "currency"
+        verbose_name_plural = "currencies"
 
 
 class CurrencyRateRecord(models.Model):
-    timestamp = models.TimeField()
-    curr = models.ForeignKey(Currency, on_delete=models.PROTECT, related_name='currency_name_records_currency')
+    """A model describing a singular record of conversion rate of a particular currency in terms of base currency."""
+    timestamp = models.DateTimeField()
+    curr = models.ForeignKey(Currency, on_delete=models.PROTECT, related_name='currency_rate_records')
     base_curr = models.ForeignKey(Currency, on_delete=models.PROTECT,
-                                  related_name='currency_name_records_base_currency')
+                                  related_name='base_currency_rate_records')
     rate = models.DecimalField()
 
     class Meta:
         app_label = "api_caller"
+        verbose_name = "currency rate record"
+        verbose_name_plural = "currency rate records"
