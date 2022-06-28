cc6177646263609e750a793c64e5453ffc692dd1
Fazzer6132 <50322696+Fazzer6132@users.noreply.github.com>
2022-06-28 15:57:56 +0300

    Update initialize_curr_table.py

--- curr_scraper/apps/api_caller/management/commands/initialize_curr_table.py
+++ curr_scraper/apps/api_caller/management/commands/initialize_curr_table.py
@@ -6,6 +6,6 @@ from curr_scraper.apps.api_caller import call_logic
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        pass
+        call_logic.initialize_currency_table()
 
-    call_logic.initialize_currency_table()
+
